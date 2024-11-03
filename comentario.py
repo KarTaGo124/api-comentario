20b5845427433441ed8fcc4f937783fc1e3ecd46
Jose Guillermo Galvez Pacori <129212989+KarTaGo124@users.noreply.github.com>
2024-11-03 18:11:35 -0500

    Update comentario.py

--- comentario.py
+++ comentario.py
@@ -1,6 +1,7 @@
 import boto3
 import uuid
 import os
+import json
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -8,18 +9,28 @@ def lambda_handler(event, context):
     tenant_id = event['body']['tenant_id']
     texto = event['body']['texto']
     nombre_tabla = os.environ["TABLE_NAME"]
+    bucket_name = os.environ["INGEST_BUCKET_NAME"]  # Asegúrate de definir esto en serverless.yml
+    
     # Proceso
     uuidv1 = str(uuid.uuid1())
     comentario = {
         'tenant_id': tenant_id,
         'uuid': uuidv1,
         'detalle': {
-          'texto': texto
+            'texto': texto
         }
     }
+    
+    # Guardar en DynamoDB
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
+    
+    # Guardar el JSON en S3
+    s3_client = boto3.client('s3')
+    s3_key = f"{tenant_id}/{uuidv1}.json"
+    s3_client.put_object(Bucket=bucket_name, Key=s3_key, Body=json.dumps(comentario))
+    
     # Salida (json)
     print(comentario)
     return {
